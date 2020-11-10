b5dea6e61466465e5058f898c8edc027d12ebc45
Robin-Neuman <robinneuman@hotmail.com>
2020-11-10 20:06:21 +0100

    added validation for vatNr

--- src/components/AddCustomerForm.jsx
+++ src/components/AddCustomerForm.jsx
@@ -3,12 +3,12 @@ import styled, { css } from 'styled-components'
 import { CustomerContext } from '../contexts/CustomerContext'
 
 
-const StyledForm = styled.form `
+const StyledForm = styled.form`
     border-radius: 7px;
     width: 50%;
     margin: 0 auto;
 `
-const StyledDiv = styled.div `
+const StyledDiv = styled.div`
     border-radius: 7px;
     border: 2px solid black;
     padding: 0.3rem;
@@ -16,7 +16,7 @@ const StyledDiv = styled.div `
     text-align: center;
 
 `
-const StyledInput = styled.input `
+const StyledInput = styled.input`
     border-radius: 7px;
     border: 2px solid black;
     background-color: CadetBlue;
@@ -28,14 +28,14 @@ const StyledInput = styled.input `
     `}
     
 `
-const StyledButton = styled.button `
+const StyledButton = styled.button`
     border-radius: 7px;
     border: 2px solid black;
     background-color: cadetBlue;
     width: 100%;
     color:black;
 `
-   
+
 const RequiredField = styled(StyledInput)`
     border: 2px solid darkred;
 `
@@ -43,30 +43,40 @@ export default function AddCustomerForm() {
 
     const addCustomerURL = 'https://frebi.willandskill.eu/api/v1/customers/'
 
+    function validateVatNr(vatNr) {
+        let splitVat = vatNr.split('-');
+        if (splitVat[0] === "SE" && splitVat[1].length === 10) {
+            return true
+        }
+    }
+
     function submitCustomer(e) {
         e.preventDefault()
-        const payload = {
-            name: e.target.name.value,
-            organisationNr: e.target.organisationNr.value || '',
-            vatNr: e.target.vatNr.value || '',
-            reference: e.target.reference.value || '',
-            paymentTerm: parseInt(e.target.paymentTerm.value) || 0,
-            website: e.target.website.value || '',
-            email: e.target.email.value || '',
-            phoneNumber: e.target.phoneNr.value || ''
+
+        if (validateVatNr(e.target.vatNr.value)) {
+            const payload = {
+                name: e.target.name.value,
+                organisationNr: e.target.organisationNr.value || '',
+                vatNr: e.target.vatNr.value || '',
+                reference: e.target.reference.value || '',
+                paymentTerm: parseInt(e.target.paymentTerm.value) || 0,
+                website: e.target.website.value || '',
+                email: e.target.email.value || '',
+                phoneNumber: e.target.phoneNr.value || ''
+            }
+            console.log(payload)
+            fetch(addCustomerURL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('loginToken')}`
+                },
+                body: JSON.stringify(payload)
+            }).then(res => {
+                window.location.reload();
+                console.log(res)
+            })
         }
-        console.log(payload)
-        fetch(addCustomerURL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${localStorage.getItem('loginToken')}`
-            },
-            body: JSON.stringify(payload)
-        }).then(res => {
-            window.location.reload();
-            console.log(res)
-        })
     }
 
     return (
